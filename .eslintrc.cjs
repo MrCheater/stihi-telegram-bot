b6265bcc58015744ce5a8a91c2e8060bd05ab5eb
mrcheater <aa_zhukov@controlsystems.ru>
2023-01-01 22:19:58 +0300

    Release

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
